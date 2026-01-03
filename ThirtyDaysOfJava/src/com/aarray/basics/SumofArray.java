aac1132860a986f47b5411c7a385f55ac57be59d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-03 21:13:39 +0530

    DAY 1 – Arrays Basics 6.	Sum of array elements Core Java logic

---
+++ ThirtyDaysOfJava/src/com/aarray/basics/SumofArray.java
@@ -0,0 +1,21 @@
+package com.aarray.basics;
+
+public class SumofArray {
+
+    static int num[] = {12, 56, 78, 25, 48, 92, 90};
+
+    static int sum()
+    {
+        int sum = 0;
+        int i;
+
+        for(i = 0; i< num.length; i++)
+            sum += num[i];
+
+        return sum;
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Summ of Array : "+sum());
+    }
+}
