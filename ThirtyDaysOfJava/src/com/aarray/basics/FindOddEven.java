eff477a3769fdcf3675becaffa89d3ee236c648c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-03 17:07:51 +0530

    DAY 1 – Arrays Basics 5. Count even & odd numbers Core Java logic

--- ThirtyDaysOfJava/src/com/aarray/basics/FindOddEven.java
+++ ThirtyDaysOfJava/src/com/aarray/basics/FindOddEven.java
@@ -2,6 +2,19 @@ package com.aarray.basics;
 
 public class FindOddEven {
     public static void main(String[] args) {
+        int [] num = {5, 80, 90, 75, 56, 35, 48, 73, 29, 19};
 
+        int checkOdd = 0, checkEven = 0;
+
+        for(int num1: num ){
+            if(num1 % 2 == 0) {
+                checkEven++;
+            }else {
+                checkOdd++;
+            }
+        }
+
+        System.out.println("Even Numbers : "+checkEven);
+        System.out.println("Odd Number : "+checkOdd);
     }
 }
