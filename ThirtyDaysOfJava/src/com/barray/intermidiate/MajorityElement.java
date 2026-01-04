ca963cca51442f8630e83ad8f2611bf0817daa9a
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-04 22:24:44 +0530

    DAY 2 – Arrays Intermediate 4.Majority element Core Java logic

---
+++ ThirtyDaysOfJava/src/com/barray/intermidiate/MajorityElement.java
@@ -0,0 +1,29 @@
+
+package com.barray.intermidiate;
+
+public class MajorityElement {
+    public static int findMajorityElement(int[] nums) {
+        int count = 1;
+        int majorityElement = nums[0];
+
+        for (int i = 1; i <nums.length; i++) {
+            if (nums[i] == majorityElement)
+                count++;
+            else
+                count--;
+
+            if (count == 0) {
+                majorityElement = nums[i];
+                count = 1;
+            }
+        }
+
+        return majorityElement;
+    }
+    public static void main(String[] args) {
+        int[] nums = {5, 5, 5, 8, 4, 4, 4};
+        int majorityElement = findMajorityElement(nums);
+        System.out.println("Majority Element : "+majorityElement);
+
+    }
+}
