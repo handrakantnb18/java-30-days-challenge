746f5628508d2342cad4cbc86c0ac192d170be7f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-04 20:24:36 +0530

    DAY 2 – Arrays Intermediate 7.Maximum subarray sum Core Java logic

---
+++ ThirtyDaysOfJava/src/com/barray/intermidiate/MaximumSubarraySum.java
@@ -0,0 +1,18 @@
+package com.barray.intermidiate;
+
+public class MaximumSubarraySum {
+    public static int maxSubArraySum(int[] array) {
+        int max1 = array[0];
+        int maxEnd = array[0];
+
+        for (int i =1; i <array.length; i++) {
+            maxEnd = Math.max(array[i], maxEnd + array[i]);
+            max1 = Math.max(max1, maxEnd);
+        }
+        return max1;
+    }
+    public static void main(String[] args) {
+        int[] array = {5, 15, 20, 44, 77, 55};
+        System.out.println("Maximum subarray sum : " );
+    }
+}
