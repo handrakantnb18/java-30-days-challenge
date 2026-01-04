ee9b2b015ab4b96ec666d4c931557fa9a2f87e51
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-04 21:31:37 +0530

    DAY 2 – Arrays Intermediate 9.Count occurrences of element Core Java logic

--- ThirtyDaysOfJava/src/com/barray/intermidiate/CountOccurrences.java
+++ ThirtyDaysOfJava/src/com/barray/intermidiate/CountOccurrences.java
@@ -2,7 +2,7 @@ package com.barray.intermidiate;
 
 public class CountOccurrences {
     public static void main(String[] args) {
-        int[] array = {9, 5, 7, 2, 8, 6, 4, 2};
+        int[] array = {9, 2, 5, 7, 2, 8, 6, 4, 2};
         int num1 = 2;
 
         int result = 0;
