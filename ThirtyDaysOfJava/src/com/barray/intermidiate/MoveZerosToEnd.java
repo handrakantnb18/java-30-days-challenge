b7921bf32f55ef2bdc355d6ba15e7f165d04a704
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-04 22:31:32 +0530

    DAY 2 – Arrays Intermediate 2.Move zeros to end Core Java logic

---
+++ ThirtyDaysOfJava/src/com/barray/intermidiate/MoveZerosToEnd.java
@@ -0,0 +1,26 @@
+
+package com.barray.intermidiate;
+
+public class MoveZerosToEnd {
+    public static void moveZerotoEnd(int[] array) {
+        int index = 0;
+
+        for (int num : array) {
+            if (num != 0) {
+                array[index++] = num;
+            }
+        }
+        while (index < array.length) {
+            array[index++] = 0;
+        }
+    }
+    public static void main(String[] args) {
+        int[] array = {1, 5, 0, 8, 7, 6, 0, 4, 0};
+        moveZerotoEnd(array);
+
+        for (int num : array) {
+            System.out.println(num + " ");
+        }
+    }
+
+}
