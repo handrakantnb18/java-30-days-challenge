c594f7e1afcf0568cc1cd857b4945eb00c8b6c63
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-05 16:31:03 +0530

    DAY 3 – Strings Basics 9.Replace character Core Java logic

---
+++ ThirtyDaysOfJava/src/com/stringsbasics/ReplaceCharacter.java
@@ -0,0 +1,11 @@
+
+package com.stringsbasics;
+
+public class ReplaceCharacter {
+    public static void main(String[] args) {
+        String str = "Welcome, hello word";
+        String replaced = str.replace('o', 'e');
+
+        System.out.println(replaced);
+    }
+}
