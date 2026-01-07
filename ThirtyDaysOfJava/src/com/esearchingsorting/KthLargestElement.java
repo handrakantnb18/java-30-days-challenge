8d89e6e7aa15eee0a32e0ed265f73d937d16ccaa
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-07 22:57:56 +0530

    DAY 5 – Searching & Sorting 10. Kth Largest Element

---
+++ ThirtyDaysOfJava/src/com/esearchingsorting/KthLargestElement.java
@@ -0,0 +1,19 @@
+
+package com.esearchingsorting;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class KthLargestElement {
+
+    public static int findKthLargest(Integer[] arr, int k) {
+        Arrays.sort(arr, Collections.reverseOrder());
+        return arr[k - 1];
+    }
+    public static void main(String[] args) {
+        Integer[] arr = {112, 5, 35, 78, 98, 29};
+        int k = 3;
+
+        System.out.println(findKthLargest(arr, k));
+    }
+}
