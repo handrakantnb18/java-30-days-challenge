34549b4b12144c5687d2269bd486adaa5a4ac063
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-07 10:23:14 +0530

    DAY 5 – Searching & Sorting 1.Linear search

---
+++ ThirtyDaysOfJava/src/com/esearchingsorting/LinearSearch.java
@@ -0,0 +1,25 @@
+package com.esearchingsorting;
+
+public class LinearSearch {
+
+    static int search(int num1[], int num2, int num3) {
+        for (int i = 0; i < num2; i++) {
+            if (num1[i] == num3)
+                return i;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] num1 = { 3, 4, 8, 7, 9, 6};
+        int num2 = num1.length;
+
+        int num3 = 4;
+
+        int index = search(num1, num2, num3);
+
+        if (index == -1)
+            System.out.println("Element is not present : ");
+        else
+            System.out.println("Element Found : " +index);
+    }
+}
