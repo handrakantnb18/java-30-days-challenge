bfa1d81d93be176c77e987e2eb992bc9f7595495
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-08 21:37:02 +0530

    DAY 6 – Linked List 9. Find length of list

---
+++ ThirtyDaysOfJava/src/com/flinkedlist/FindLengthofList.java
@@ -0,0 +1,20 @@
+package com.flinkedlist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindLengthofList {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Cricket");
+        list.add("Footboll");
+        list.add("Kabbadi");
+        list.add("Vhaliboll");
+        list.add("Kho-Kho");
+        list.add("CaramBoard");
+        list.add("Table Tenis");
+
+        int size = list.size();
+        System.out.println("List size : " + size);
+    }
+}
