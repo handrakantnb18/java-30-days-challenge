e71c87ad0975e398c7971a3c43a8902fcf232596
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-08 18:11:53 +0530

    DAY 6 – Linked List 6. Reverse linked list

---
+++ ThirtyDaysOfJava/src/com/flinkedlist/ReverseLinkedList.java
@@ -0,0 +1,23 @@
+
+package com.flinkedlist;
+
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(2);
+        linkedList.add(3);
+        linkedList.add(4);
+        linkedList.add(5);
+        linkedList.add(6);
+
+        System.out.println("Before Reverse : " +linkedList);
+        Collections.reverse(linkedList);
+
+        System.out.println("After Reverse : " + linkedList);
+    }
+}
