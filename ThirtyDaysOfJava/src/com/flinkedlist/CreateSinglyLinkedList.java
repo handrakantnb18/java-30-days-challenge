010ba6fd4799861bda894cbacf27660cd2fd8e4d
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-08 10:30:43 +0530

    DAY 6 – Linked List 1.	Create singly linked list

--- ThirtyDaysOfJava/src/com/flinkedlist/CreateSinglyLinkedList.java
+++ ThirtyDaysOfJava/src/com/flinkedlist/CreateSinglyLinkedList.java
@@ -1,6 +1,32 @@
 package com.flinkedlist;
 
+import org.w3c.dom.Node;
+
 public class CreateSinglyLinkedList {
+
+    int data;
+    Node Next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = next;
+    }
+
+    private Node head = null;
+
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+            if (head == null){
+                head = newNode;
+            }else {
+                Node current = head;
+                while (current.next != null) {
+                    current = current.next;
+                }
+                current.next = newNode;
+            }
+    }
+
 
     public static void main(String[] args) {
 
