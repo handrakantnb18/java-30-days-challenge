2cac786c8d6a3a7fb1a713a902e80e382c7288bb
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-09 10:59:58 +0530

    DAY 7 – Linked List (Advanced) 2.Find nth node from end

---
+++ ThirtyDaysOfJava/src/com/glinkedlistadvanced/FindnthNodeFromend.java
@@ -0,0 +1,43 @@
+
+package com.glinkedlistadvanced;
+
+class NodeFormEnd {
+    int data;
+    NodeFormEnd next;
+
+    NodeFormEnd(int new_data){
+        data = new_data;
+        next = null;
+    }
+}
+
+public class FindnthNodeFromend {
+    static int findNthFormLast(NodeFormEnd head, int N) {
+        int len = 0, i;
+
+        NodeFormEnd temp = head;
+
+        while (temp != null) {
+            temp = temp.next;
+            len++;
+        }
+        if (len < N) {
+            return -1;
+        }
+        temp = head;
+
+        for (i = 1; i < len - N + 1; i++)
+            temp = temp.next;
+
+        return temp.data;
+    }
+    public static void main(String[] args) {
+
+        NodeFormEnd head = new NodeFormEnd(50);
+        head.next = new NodeFormEnd(75);
+        head.next.next = new NodeFormEnd(85);
+        head.next.next.next = new NodeFormEnd(5);
+
+        System.out.println(findNthFormLast(head, 4));
+    }
+}
