7ddfd1bcbfbd8cc8e92d59ce6a8ac2a1af5a0c2c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-09 10:20:39 +0530

    DAY 7 – Linked List (Advanced) 1.Remove duplicates

---
+++ ThirtyDaysOfJava/src/com/glinkedlistadvanced/RemoveDuplicates.java
@@ -0,0 +1,20 @@
+package com.glinkedlistadvanced;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+public class RemoveDuplicates {
+    public static int[] removeDeplicates(int[] array) {
+        LinkedHashSet<Integer> lhs = new LinkedHashSet<>();
+        for (int num : array) {
+            lhs.add(num);
+        }
+        return lhs.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 2, 3, 4, 4, 5, 5, 8, 8 ,7};
+        int[] result = removeDeplicates(array);
+        System.out.println(Arrays.toString(result));
+    }
+}
