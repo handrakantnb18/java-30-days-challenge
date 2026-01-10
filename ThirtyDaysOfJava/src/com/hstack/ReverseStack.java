ae1c508755efee41b650392b167bad38d4d2d3b8
Chandrakant bhosale <45489110+handrakantnb18@users.noreply.github.com>
2026-01-10 13:51:35 +0530

    DAY 8 – Stack 1. Implement stack using array

--- ThirtyDaysOfJava/src/com/hstack/ReverseStack.java
+++ ThirtyDaysOfJava/src/com/hstack/ReverseStack.java
@@ -1,9 +1,41 @@
 package com.hstack;
 
+import java.util.*;
 public class ReverseStack {
 
-    public static void main(String[] args) {
-    ReverseStack rs = new ReverseStack();
+    static  void insertAtBottom(Stack<Integer> st, int x) {
+        if (st.isEmpty()) {
+            st.push(x);
+            return;
+        }
+        int top = st.pop();
+        insertAtBottom(st, x);
+        st.push(top);
 
+    }
+
+    static void reverseStack(Stack<Integer> st){
+        if (st.isEmpty())
+            return;
+
+        int top = st.pop();
+
+        reverseStack(st);
+        insertAtBottom(st, top);
+    }
+    public static void main(String[] args) {
+    Stack<Integer> rs = new Stack<>();
+    rs.push(1);
+    rs.push(2);
+    rs.push(3);
+    rs.push(4);
+    rs.push(5);
+    rs.push(6);
+
+    reverseStack(rs);
+
+    while (!rs.isEmpty()) {
+        System.out.println(rs.pop() + " ");
+    }
     }
 }
