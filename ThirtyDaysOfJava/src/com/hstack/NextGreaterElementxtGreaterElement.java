dcd34b902478920a72aad72550c75301584a81de
Chandrakant bhosale <45489110+handrakantnb18@users.noreply.github.com>
2026-01-10 15:40:08 +0530

    Day 8 : Stack  6.Next greater element

---
+++ ThirtyDaysOfJava/src/com/hstack/NextGreaterElementxtGreaterElement.java
@@ -0,0 +1,23 @@
+package com.hstack;
+
+public class NextGreaterElementxtGreaterElement {
+
+    static void printNGE(int arr[], int n) {
+        int next, i, j;
+        for (i = 0; i < n; i++) {
+            next = -1;
+            for (j = i+1; j < n; j++) {
+                if (arr[i] < arr[j]) {
+                    next = arr[j];
+                    break;
+                }
+            }
+            System.out.println(arr[i] + " -- " + next);
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {10, 5, 25, 3, 78, 1, 63};
+        int n = arr.length;
+        printNGE(arr, n);
+    }
+}
