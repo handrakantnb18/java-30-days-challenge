c0959c727c67f717f1638bc662498b36e729a9c1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-12 20:11:00 +0530

    DAY 10 – HashMap / HashSet 3.	First repeating element

---
+++ ThirtyDaysOfJava/src/com/jhashMapHashSet/FirstRepeatingElement.java
@@ -0,0 +1,26 @@
+package com.jhashMapHashSet;
+
+public class FirstRepeatingElement {
+    static  int firstRepeatingEle(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j])
+                    return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 2, 5, 3, 4, 3, 5, 6};
+        int n = arr.length;
+
+        int index =firstRepeatingEle(arr, n);
+
+        if (index == -1) {
+            System.out.println("No repeating element found...");
+        }
+        else {
+            System.out.println("First repeating element found... " +arr[index]);
+        }
+    }
+}
