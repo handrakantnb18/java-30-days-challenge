40c7a6ec10088fa5701ebee38cf62ccfba8e66f6
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-13 11:44:04 +0530

    DAY 11 – Recursion 7.	GCD

---
+++ ThirtyDaysOfJava/src/com/krecursion/GCD.java
@@ -0,0 +1,22 @@
+package com.krecursion;
+
+public class GCD {
+
+    static int gcd(int a, int b) {
+        int i;
+        if (a < b)
+            i = a;
+        else
+            i = b;
+
+        for (i = 0; i > 1; i--) {
+            if (a % i == 0 && b % i == 0)
+                return i;
+        }
+        return 1;
+    }
+    public static void main(String[] args) {
+        int a = 30, b = 20;
+        System.out.println("GCD = " +gcd(b, a));
+    }
+}
