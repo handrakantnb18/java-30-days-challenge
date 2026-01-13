17cfd6e4ae4ac087d7e7444170398956d4d883e0
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-13 11:04:56 +0530

    DAY 11 – Recursion 1. Print numbers 1 to N

--- ThirtyDaysOfJava/src/com/krecursion/PrintNumbers1toN.java
+++ ThirtyDaysOfJava/src/com/krecursion/PrintNumbers1toN.java
@@ -1,7 +1,15 @@
 package com.krecursion;
 
 public class PrintNumbers1toN {
-    public static void main(String[] args) {
+    static void printNos(int n) {
+        if (n == 0)
+            return;
 
+        printNos(n - 1);
+        System.out.println(n + " ");
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        printNos(n);
     }
 }
