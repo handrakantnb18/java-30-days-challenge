c90a5bcd8ed1938bfa7f522ab74699f2c046867c
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-13 10:52:24 +0530

    DAY 9 – Queue 9. Generate binary numbers

---
+++ ThirtyDaysOfJava/src/com/iqueue/GenerateBinaryNumbers.java
@@ -0,0 +1,24 @@
+package com.iqueue;
+
+public class GenerateBinaryNumbers {
+    static void printBinary(int[] binary, int id) {
+        for (int i = id - 1; i >= 0; i--)
+            System.out.println(binary[i]+ " ");
+    }
+
+    public static void decimalToBinary(int num) {
+        int[] binary = new int[35];
+        int id = 0;
+
+        while (num > 0){
+            binary[id++] = num % 2;
+            num = num / 2;
+        }
+        printBinary(binary, id);
+    }
+
+    public static void main(String[] args) {
+        int num = 45;
+        decimalToBinary(num);
+    }
+}
