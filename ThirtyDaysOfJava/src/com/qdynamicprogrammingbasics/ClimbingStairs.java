287994af22a14d709fcd5ec937240ca37a657c66
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-19 10:27:14 +0530

    DAY 17 – Dynamic Programming (Basics) 2. Climbing stairs

---
+++ ThirtyDaysOfJava/src/com/qdynamicprogrammingbasics/ClimbingStairs.java
@@ -0,0 +1,27 @@
+package com.qdynamicprogrammingbasics;
+
+import java.util.Arrays;
+
+public class ClimbingStairs {
+    static  int countWayRec(int n, int[] memo) {
+
+        if (n == 0 || n == 1)
+            return 1;
+
+        if (memo[n] != -1)
+            return memo[n];
+
+        return memo[n] = countWayRec(n - 1, memo) + countWayRec(n - 2, memo);
+    }
+
+    static int countWays(int n) {
+        int[] memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+
+        return countWayRec(n, memo);
+    }
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(countWays(n));
+    }
+}
--- ThirtyDaysOfJava/src/com/qdynamicprogrammingbasics/FibonacciDP.java
+++ ThirtyDaysOfJava/src/com/qdynamicprogrammingbasics/FibonacciDP.java
@@ -10,7 +10,7 @@ public class FibonacciDP {
     }
     public static void main(String[] args) {
 
-        int N = 10;
+        int N = 5;
 
         for (int i = 0; i < N; i++) {
             System.out.println(fibonecii(i) + " ");
