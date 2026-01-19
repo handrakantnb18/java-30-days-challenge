a3e31afff7b0287e43a969c4fc3deb52c509ebc8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-19 21:46:04 +0530

    DAY 17 – Dynamic Programming (Basics) 10. Count ways to reach N

---
+++ ThirtyDaysOfJava/src/com/qdynamicprogrammingbasics/CountWaystoReachN.java
@@ -0,0 +1,24 @@
+package com.qdynamicprogrammingbasics;
+
+public class CountWaystoReachN {
+    static int countWaysUnits(int n, int m) {
+        int res[] = new int[n];
+        res[0] = 1;
+        res[1] = 1;
+        for (int i= 2; i < n; i++){
+            res[i] =0;
+            for (int j = 1; j <= m && j <= i; j ++)
+                res[i] += res[i - j];
+        }
+        return res[n - 1];
+    }
+
+    static int countWays(int s, int m) {
+        return countWaysUnits(s + 1, m);
+    }
+
+    public static void main(String[] args) {
+        int s = 4, m = 2;
+        System.out.println("Number of ways :  " +countWays(s, m));
+    }
+}
