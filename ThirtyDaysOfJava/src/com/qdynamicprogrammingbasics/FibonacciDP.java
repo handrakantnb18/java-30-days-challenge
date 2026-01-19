00f06e41cac0f9bba69c42b527e3fdcfd7960696
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-19 10:01:32 +0530

    DAY 17 – Dynamic Programming (Basics) 1.Fibonacci DP

---
+++ ThirtyDaysOfJava/src/com/qdynamicprogrammingbasics/FibonacciDP.java
@@ -0,0 +1,19 @@
+package com.qdynamicprogrammingbasics;
+
+public class FibonacciDP {
+
+    static int fibonecii(int n) {
+        if (n <= 1)
+            return n;
+
+        return fibonecii(n - 1) + fibonecii(n - 2);
+    }
+    public static void main(String[] args) {
+
+        int N = 10;
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(fibonecii(i) + " ");
+        }
+    }
+}
