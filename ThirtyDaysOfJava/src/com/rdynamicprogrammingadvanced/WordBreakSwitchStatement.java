2daec9ff1185e99222f5d910ed27b827af204122
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-20 21:39:40 +0530

    DAY 18 – Dynamic Programming (Advanced) 8. Word break

---
+++ ThirtyDaysOfJava/src/com/rdynamicprogrammingadvanced/WordBreakSwitchStatement.java
@@ -0,0 +1,35 @@
+package com.rdynamicprogrammingadvanced;
+
+public class WordBreakSwitchStatement {
+    public static void main(String[] args) {
+        int n = 1;
+
+        switch(n) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wendsday");
+                break;
+            case 4:
+                System.out.println("Thrusday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("No days found");
+        }
+    }
+
+
+}
