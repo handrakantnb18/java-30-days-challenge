454b76ccad90cd4a5a799b6db585c3cd523c0a02
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-20 19:26:14 +0530

    DAY 18 – Dynamic Programming (Advanced) 4. Matrix chain multiplication

---
+++ ThirtyDaysOfJava/src/com/rdynamicprogrammingadvanced/MatrixChainMultiplication.java
@@ -0,0 +1,39 @@
+package com.rdynamicprogrammingadvanced;
+
+import java.util.Arrays;
+
+public class MatrixChainMultiplication {
+    static int[][] dp = new int[100][100];
+
+    static int matrixChainMemoised(int[] p, int i, int j) {
+        if (i == j){
+            return 0;
+        }
+        if (dp[i][j] != -1){
+            return dp[i][j];
+        }
+        dp[i][j] = Integer.MAX_VALUE;
+
+        for (int k = i; k < j; k++) {
+            dp[i][j] = Math.min(
+                    dp[i][j], matrixChainMemoised(p, i, k)
+                    +p[i - 1] * p[k] * p[j]);
+        }
+        return dp[i][j];
+    }
+
+    static int MatrixChainOrder(int[] p, int n)
+    {
+        int i = 1, j = n -1;
+        return matrixChainMemoised(p, i, j);
+    }
+    public static void main(String[] args) {
+
+        int arr[] = { 1, 2, 3, 4};
+        int n = arr.length;
+
+        for (int[] row : dp)
+            Arrays.fill(row, -1);
+        System.out.println("Minimum number of multiplication is : "+MatrixChainOrder(arr, n));
+    }
+}
