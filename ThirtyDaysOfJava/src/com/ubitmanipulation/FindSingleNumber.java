6fab289fc4aff74772c244b0ab772f25ee579fbd
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-23 11:11:27 +0530

    DAY 21 – Bit Manipulation 3. Find single number

---
+++ ThirtyDaysOfJava/src/com/ubitmanipulation/FindSingleNumber.java
@@ -0,0 +1,22 @@
+package com.ubitmanipulation;
+
+public class FindSingleNumber {
+    public static int missingNum(int[] arr) {
+        int n = arr.length + 1;
+        int m1 = 0, m2 = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            m2 ^= arr[i];
+        }
+
+        for (int i = 1; i <= n; i++) {
+            m1 ^= i;
+        }
+        return m1 ^ m2;
+    }
+    public static void main(String[] args) {
+        int[] arr = {8, 2, 4, 5, 3, 7, 1};
+        int res = missingNum(arr);
+        System.out.println(res);
+    }
+}
