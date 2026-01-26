f5f5fcf211fbcec35bf33466fbca6fe2c22fd6b1
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-26 13:41:18 +0530

    DAY 24 – Java 8  4. Reduce example

---
+++ ThirtyDaysOfJava/src/com/xjava8/GroupingBy.java
@@ -0,0 +1,22 @@
+package com.xjava8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class GroupingBy {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList(
+                "Priyanka", "Chandrakant", "Bhosale", "Sangli", "pune"
+        );
+
+        Map<String, Long> result = list.stream().collect(
+                Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        System.out.println(result);
+
+
+    }
+}
