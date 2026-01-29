edcbaad9c2bec18c3a3f2d50cf95c6434adb3e1f
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-01-29 13:04:46 +0530

    DAY 27 – Arrays (Interview Level) 8. 3 Sum

---
+++ ThirtyDaysOfJava/src/com/zaarraysInterviewlevel/Three3Sum.java
@@ -0,0 +1,26 @@
+package com.zaarraysInterviewlevel;
+
+public class Three3Sum {
+
+    public static void main(String[] args) {
+        // This program demonstrates how to use the Integer.sum() method to add two integer values in Java.
+        int a = 18;
+        int b = 13;
+        System.out.println("The sum is : " + Integer.sum(a, b));
+
+        //  Returns an int value which is the sum of the two arguments.
+        int c = 1809900018;
+        int d = 1819;
+        System.out.println("Sum of int : " + Integer.sum(c, d));
+
+        // Java throws a compile-time error before execution.
+
+        int[] arr = { 2, 4, 6, 8, 1, 12, 14, 16, 18};
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++){
+            sum = Integer.sum(sum, arr[i]);
+        }
+        System.out.println("Sum of the array elements is : "+ sum);
+    }
+}
