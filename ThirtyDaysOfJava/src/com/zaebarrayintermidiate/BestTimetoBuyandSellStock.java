c9df5ff0057bfb415df9be9f8a07385897f10007
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-02 23:14:13 +0530

    DAY 2 – barray intermidiate 1. Time to Buyand Sell Stock

---
+++ ThirtyDaysOfJava/src/com/zaebarrayintermidiate/BestTimetoBuyandSellStock.java
@@ -0,0 +1,27 @@
+package zaebarrayintermidiate;
+
+public class BestTimetoBuyandSellStock {
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+            int profit = prices[i] - minPrice;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int[] stockPrices = {7, 8, 9, 5, 4, 6};
+        int result = maxProfit(stockPrices);
+        System.out.println("Mazimum profit : "+result);
+    }
+}
