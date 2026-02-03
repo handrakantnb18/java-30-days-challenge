c198bb38b27583408ef6fbe0a1bf58ddeb585ed8
Chandrakantbhosale <chandrakantnb18@gmail.com>
2026-02-03 21:20:25 +0530

    DAY 3 – String Advance 3. Count Words

---
+++ ThirtyDaysOfJava/src/com/zafstringadvance/CountWords.java
@@ -0,0 +1,14 @@
+package com.zafstringadvance;
+
+public class CountWords {
+    public static void main(String[] args) {
+
+        String string = "Chandrakant bhosale Pune software developer";
+        if (string == null || string.trim().isEmpty()) {
+            System.out.println(0);
+        }else {
+            String[] str = string.trim().split("\\s+");
+            System.out.println("Number of words : " +str.length);
+        }
+    }
+}
